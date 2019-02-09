9255c7cf1879013607cb42f6dd492fc2ed25d309
Gavin Barnett <gavbarnett@gmail.com>
2019-02-09 19:25:10 +0000

    MrParsy directory ninja
    MrParsy can now operate over a hole directory (1 level deep). will convert all csv files in sight to jsons.

--- MrParsy.py
+++ MrParsy.py
@@ -1,23 +1,31 @@
-import os, argparse, csv, json
+import os, argparse, csv, json, glob
 #from pathlib import Path
 aparser = argparse.ArgumentParser(description='convert csv to json')
 aparser.add_argument(
     '-f', '--file', dest='fname', action='store',
     help='file name for csv to be parsed')
+aparser.add_argument(
+    '-d', '--dir', dest='dname', action='store',
+    help='directory name for all csv files to be parsed')
 args = aparser.parse_args()
 
 def main ():
+	format = "pretty"
 	if (args.fname): 
 		input_file = args.fname
+		output_file = input_file[:len(input_file)-3] + "json"
+		read_csv(input_file, output_file, format)
+	elif (args.dname):
+		input_directory = args.dname
+		if ((input_directory[-1:] != '\\') & (input_directory[-1:] != '/')):
+			input_directory = input_directory + "\\"
+		file_list = glob.iglob(input_directory +'*.csv')
+		for input_file in file_list:
+			output_file = input_file[:len(input_file)-3] + "json"
+			read_csv(input_file, output_file, format)
 	else:
 		print ('ERROR: no file given to parse')
 		exit()
-	#read csv file
-	#input_file = csv.DictReader(open(fname, "r"), delimiter = ";")
-
-	output_file = input_file[:len(input_file)-3] + "json"
-	format = "pretty"
-	read_csv(input_file, output_file, format)
 
 def read_csv(file, json_file, format):
 	csv_rows = []
@@ -33,6 +41,7 @@ def write_json(data, json_file, format):
 		if format == "pretty":
 			f.write(json.dumps(data, sort_keys=False, indent=4,))
 		else:
-			f.write(json.dumps(data))		
+			f.write(json.dumps(data))
+		print(json_file + " - complete")		
 
 main()
