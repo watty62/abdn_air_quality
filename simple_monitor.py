f0833cb6c956afdb08249274df97eb7aec84be83
Gavin Barnett <gavbarnett@gmail.com>
2019-03-09 13:57:58 +0000

    tidy dictionary
    the tidy function returns a more usable dictionary. all sensor on a unit are group together

--- simple_monitor.py
+++ simple_monitor.py
@@ -2,6 +2,7 @@
 
 import requests
 from datetime import datetime
+import pprint 
 
 def get_data(box = '57.5476,-1.9113,56.9630,-2.4682'):
     #gets luftdaten data for all sensors within a givin lat/log box
@@ -12,6 +13,29 @@ def get_data(box = '57.5476,-1.9113,56.9630,-2.4682'):
     my_json = r.json()
     return my_json
 
+def tidy_values(our_list):
+    #organises ourlist as a dictionary of dictionaries follows:
+    #{location_id:{
+    #   id: [a,b],
+    #   humidity: value,
+    #   temperature: value   
+    #   }
+    # }
+    new_dict = {}
+    for sensor in our_list:
+            location_id = str(sensor['location']['id'])
+            if (new_dict.get(location_id, None)==None):
+                new_dict[location_id] = {}
+                new_dict[location_id]['location'] = sensor['location']
+            for sensordata in sensor['sensordatavalues']:
+                new_dict[location_id][sensordata['value_type']] = {
+                    'value' : float(sensordata['value']),
+                    'timestamp' : sensor['timestamp'],
+                    'id':sensor['id'],
+                    'sensor_type' : sensor['sensor']['sensor_type']}
+    pp = pprint.PrettyPrinter(indent=1)
+    pp.pprint (new_dict)
+    return(new_dict)
 
 def extract_values(our_list):
     for sensor in our_list:
@@ -23,7 +47,7 @@ def test_values(our_list):
     for sensor in our_list:
         #set up check variable
         check = {}
-        check['id'] = Device_ID = sensor['id']
+        check['id'] = sensor['id']
         check['timestamp'] = False
         check['humidity'] = False
         check['temperature'] = False
@@ -44,10 +68,10 @@ def test_values(our_list):
 def main():
     box = '57.5476,-1.9113,56.9630,-2.4682'
     our_list = get_data(box)
-    # our_list is a list of dictionaries
-    extract_values(our_list)
+    #extract_values(our_list)
+    tidy_list = tidy_values(our_list)
+    #our_list is a list of dictionaries
     #test_values(our_list)
-
 
 if __name__ == '__main__':
     main()
