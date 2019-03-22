18f4ab5fc21522ddb20b62833b42c3acb796967b
watty62 <ianwatt@gmail.com>
2019-03-22 18:10:22 +0000

    add tweeting

--- Is_it_still_up.py
+++ Is_it_still_up.py
@@ -23,7 +23,7 @@ def get_sensor_dict():
 
 
 def query_the_api():
-    """ Using a bounding box, retrieves a list containing the json of sensor devices found"""
+    """ Using a bounding box, retrieve a json list of sensor devices found"""
 
     box = [57.74, -2.97, 56.4, -1.7]
     strbox = (str(box)[1:-1]).replace(" ", "")
@@ -58,17 +58,18 @@ def lost_new_stillthere(existing, found, ex_dic, dat_tim):
     unchanged = list(exist_set & found_set)
     new_found = list(found_set - exist_set)
 
+    # print (f"Unchanged = {unchanged}")
+    # print(f"new_found = {new_found}")
     update_stored(unchanged, new_found, ex_dic, dat_tim)
 
     lost = list(exist_set - found_set)
-    print(lost)
+
     if len(lost) > 0:
+        # print(lost)
         send_alerts_if_down(lost)
 
 
 def lookup_host(sensor_id):
-    """ check our stored list of hosts' contact details and return an email address if a match found."""
-
     h_email = ""
     with open('hosts_contacts.json') as json_file:
         host_list = json.load(json_file)
@@ -97,14 +98,36 @@ def update_stored(unchanged, new_found, ex_dic, dat_tim):
         new_dict['first_seen'] = str(right_now)
         new_dict['last_seen'] = str(right_now)
         ex_dic['devices'].append(new_dict)
-
+    if len(new_found) > 0:
+        send_tweet(new_found)
+        pass
     with open('last_seen.json', 'w') as out_file:
         json.dump(ex_dic, out_file, indent=4)
 
 
-def send_alerts_if_down(in_list):
-    """ Sends an email to the contact address for each sensor which is reported as being down"""
+def send_tweet(in_list):
+    """Receive a list of new devices. Send a tweet for each."""
 
+    with open('credentials.json') as json_file:
+        creds = json.load(json_file)
+
+    twit_creds = creds['twitter']
+    consumer_key = twit_creds['consumer_key']
+    consumer_secret = twit_creds['consumer_secret']
+    access_token = twit_creds['access_token']
+    access_token_secret = twit_creds['access_token_secret']
+
+    for dev in in_list:
+        to_tweet = f"New #Aberdeen AQ device found. ID = {dev}. See it on a map: http://uk.maps.luftdaten.info/#9/57.3406/-1.9226 "
+        # tweet the message
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        tweepyapi = tweepy.API(auth)
+        tweepyapi.update_status(to_tweet)
+        # print("Tweeted")
+
+
+def send_alerts_if_down(in_list):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     sender_email = "airaberdeen@gmail.com"
@@ -114,33 +137,31 @@ def send_alerts_if_down(in_list):
     gmail_creds = creds['gmail']
     password = gmail_creds['password']
 
-    print(type(in_list))
-    print(in_list)
+    # print (type(in_list))
+    # print (in_list)
 
     for sens in in_list:
+        receiver_email = lookup_host(sens)  # get recipient address
+        sensor_id = str(sens)
+        # Todo add last_seen date_time
 
-        if str(sens).lower() != 'unknown'
-            receiver_email = lookup_host(sens)  # get recipient address
-            sensor_id = str(sens)
-            # Todo add last_seen date_time
+        message = f"""\
+        Subject: It looks like your Air Quality sensor {sensor_id} is offline
 
-            message = f"""\
-            Subject: It looks like your Air Quality sensor {sensor_id} is offline
-    
-            TEST EMAIL PLEASE IGNORE
-    
-            Our automated test has detected that your AQ device {sensor_id} has gone offline. It was last seen more than an hour ago.
-    
-            See http://deutschland.maps.luftdaten.info/#10/57.1568/-1.9007 
-    
-            Please check if it needs rebooted, or if it has a power or wifi problem.
-    
-            The Aberdeen AQ team. """
+        TEST EMAIL PLEASE IGNORE
 
-            context = ssl.create_default_context()
-            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-                server.login(sender_email, password)
-                server.sendmail(sender_email, receiver_email, message)
+        Our automated test has detected that your AQ device {sensor_id} has gone offline. It was last seen more than an hour ago.
+
+        See http://deutschland.maps.luftdaten.info/#10/57.1568/-1.9007 
+
+        Please check if it needs rebooted, or if it has a power or wifi problem.
+
+        The Aberdeen AQ team. """
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
 
 
 def main():
